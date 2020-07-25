e8c78f23df900bd641d22f71d8e27b8fa38b6d9f
ComputerOnFire <computeronfire@protonmail.com>
2020-07-25 04:51:31 -0500

    armhf/64 merge and refactor

--- scripts.d/20_tiv.sh
+++ scripts.d/20_tiv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clone https://github.com/stefanhaustein/TerminalImageViewer.git
-if uname -m | grep -q "aarch64" ;then
+if uname -m | grep -q "aarch64" ; then
     CXX=aarch64-linux-gnu-g++ make -C TerminalImageViewer/src/main/cpp
     mv TerminalImageViewer/src/main/cpp/tiv mnt/img_root/usr/local/bin/
 else
