41c7908610f60203323b67008ccc6ae2887619ce
ComputerOnFire <computeronfire@protonmail.com>
2020-07-30 00:01:52 -0500

    fix node, remove node from drone yml

--- scripts.d/20_fix_node.sh
+++ scripts.d/20_fix_node.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 source lib.sh
-
-if uname -m | grep -q "aarch64" ; then
-    exit 0
-fi
-
 echo "Fix node"
-# armv6l
-wget https://nodejs.org/dist/v10.16.0/node-v10.16.0-linux-armv6l.tar.gz
-tar xvzf node-v10.16.0-linux-armv6l.tar.gz node-v10.16.0-linux-armv6l/bin/node
-mv node-v10.16.0-linux-armv6l/bin/node mnt/img_root/usr/bin/node-armv6l
-_op _chroot chown root:root /usr/bin/node-armv6l
-rm -rf node-v10.16.0-linux-armv6l
-
-# armv7l
-_op _chroot mv /usr/bin/node /usr/bin/node-armv7l
-_op _chroot ln -sr /usr/bin/node-armv7l /usr/bin/node
+if uname -m | grep -q "aarch64" ; then
+  wget -N https://nodejs.org/dist/v10.16.0/node-v10.16.0-linux-arm64.tar.xz
+  tar xvzf node-v10.16.0-linux-arm64.tar.gz node-v10.16.0-linux-arm64/bin/node
+  mv node-v10.16.0-linux-arm64/bin/node mnt/img_root/usr/bin/node
+  _op _chroot chown root:root /usr/bin/node
+  rm -rf node-v10.16.0-linux-arm64
+else
+  # armv6l
+  wget -N https://nodejs.org/dist/v10.16.0/node-v10.16.0-linux-armv6l.tar.gz
+  tar xvzf node-v10.16.0-linux-armv6l.tar.gz node-v10.16.0-linux-armv6l/bin/node
+  mv node-v10.16.0-linux-armv6l/bin/node mnt/img_root/usr/bin/node-armv6l
+  _op _chroot chown root:root /usr/bin/node-armv6l
+  rm -rf node-v10.16.0-linux-armv6l
+  # armv7l
+  _op _chroot mv /usr/bin/node /usr/bin/node-armv7l
+  _op _chroot ln -sr /usr/bin/node-armv7l /usr/bin/node
+fi
