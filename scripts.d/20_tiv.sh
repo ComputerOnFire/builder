a0c01bad485f7f9a680cfbf173fa852b2cd4bd73
ComputerOnFire <computeronfire@protonmail.com>
2020-07-29 23:48:57 -0500

    remove echo

--- scripts.d/20_tiv.sh
+++ scripts.d/20_tiv.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 git clone https://github.com/stefanhaustein/TerminalImageViewer.git
-echo $(uname -m)
 if uname -m | grep -q "aarch64" ; then
     CXX=aarch64-linux-gnu-g++ make -C TerminalImageViewer/src/main/cpp
     mv TerminalImageViewer/src/main/cpp/tiv mnt/img_root/usr/local/bin/
