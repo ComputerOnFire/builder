2b1bae69c0e143330b14ffa073d92cd898b735ec
ComputerOnFire <computeronfire@protonmail.com>
2020-08-03 14:16:19 -0500

    try docker inside image

--- scripts.d/30_docker.sh
+++ scripts.d/30_docker.sh
@@ -14,47 +14,47 @@ MULTIS=(
     treehouses/planet:db-init
 )
 
-OLD=$(pwd -P)
-cd /var/lib || die "ERROR: /var/lib folder doesn't exist, exiting"
+#OLD=$(pwd -P)
+#cd /var/lib || die "ERROR: /var/lib folder doesn't exist, exiting"
 
-service docker stop
-mv docker docker.temp
-mkdir -p "$OLD/mnt/img_root/var/lib/docker"
-ln -s "$OLD/mnt/img_root/var/lib/docker" docker
-service docker start
+#service docker stop
+#mv docker docker.temp
+#mkdir -p "$OLD/mnt/img_root/var/lib/docker"
+#ln -s "$OLD/mnt/img_root/var/lib/docker" docker
+#service docker start
 
 for image in "${IMAGES[@]}" ; do
-    docker pull "$image"
+    _op _chroot docker pull "$image"
 done
 
-mkdir -p ~/.docker
-echo '{"experimental": "enabled"}' > ~/.docker/config.json
+_op _chroot mkdir -p ~/.docker
+_op _chroot echo '{"experimental": "enabled"}' > ~/.docker/config.json
 
-mkdir -p "$OLD/mnt/img_root/root/.docker"
-cp ~/.docker/config.json "$OLD/mnt/img_root/root/.docker/."
+#mkdir -p "$OLD/mnt/img_root/root/.docker"
+#cp ~/.docker/config.json "$OLD/mnt/img_root/root/.docker/."
 
 for multi in "${MULTIS[@]}" ; do
-    docker manifest inspect "$multi"
+    _op _chroot docker manifest inspect "$multi"
     name=$(echo "$multi" | cut -d ":" -f 1)
     tag=$(echo "$multi" | cut -d ":" -f 2)
     hash=$(docker manifest inspect "$multi" | jq '.manifests' | jq -c 'map(select(.platform.architecture | contains("arm")))' | jq '.[0]' | jq '.digest' | sed -e 's/^"//' -e 's/"$//')
-    docker pull "$name@$hash"
-    docker tag "$name@$hash" "$name:$tag" 
+    _op _chroot docker pull "$name@$hash"
+    _op _chroot docker tag "$name@$hash" "$name:$tag" 
 done
 
-docker tag treehouses/planet:db-init treehouses/planet:db-init-local
-docker tag treehouses/planet:latest treehouses/planet:local
+_op _chroot docker tag treehouses/planet:db-init treehouses/planet:db-init-local
+_op _chroot docker tag treehouses/planet:latest treehouses/planet:local
 
-sync; sync; sync
+_op _chroot sync; _op _chroot sync; _op _chroot sync
 
-docker images
+_op _chroot docker images
 
-service docker stop
-unlink docker
-mv docker.temp docker
-service docker start
+#_op _chroot service docker stop
+#unlink docker
+#mv docker.temp docker
+#service docker start
 
-cd "$OLD" || die "ERROR: $OLD folder doesn't exist, exiting"
+#cd "$OLD" || die "ERROR: $OLD folder doesn't exist, exiting"
 
 _op _chroot adduser pi docker
 
