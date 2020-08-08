c31ebb8bf8506791ff150053f11d4d0bd5ab2661
ComputerOnFire <computeronfire@protonmail.com>
2020-08-07 23:19:12 -0500

    fix install directory

---
+++ scripts.d/20_install_autorun.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source lib.sh
+
+cat << EOF > mnt/img_root/etc/systemd/system/autorun.service
+[Unit]
+Description=run autorun(once)(.sh) scripts from USB stick or in /boot
+After=local_fs.target remote_fs.target network-online.target
+
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+ExecStart=/usr/local/bin/do_autorun start
+Restart=no
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+
+mkdir -p "mnt/img_root/etc/systemd/system/multi-user.target.wants"
+_op _chroot ln -sr mnt/img_root/etc/systemd/system/autorun.service mnt/img_root/etc/systemd/system/multi-user.target.wants/autorun.service
