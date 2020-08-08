008cc09f488bf06b82d6e545ff413415967015fa
ComputerOnFire <computeronfire@protonmail.com>
2020-08-08 02:25:41 -0500

    revert to old install folder

--- scripts.d/20_install_files.sh
+++ scripts.d/20_install_files.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#source lib.sh
+source lib.sh
 echo "Installing files"
-
-exit 0
-
-#(cd install || die "ERROR: install folder doesn't exist, exiting"; tar c .) | _op _chroot tar vx --owner=root --group=root
+(cd install || die "ERROR: install folder doesn't exist, exiting"; tar c .) | _op _chroot tar vx --owner=root --group=root
